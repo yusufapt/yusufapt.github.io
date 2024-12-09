c325744a8135b36e9d3e6378c1fccb0a020ccf29
yusufapt <123107740+yusufapt@users.noreply.github.com>
2024-12-09 18:01:02 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,112 +1,24 @@
-var radius = 240;
-var autoRotate = true; 
-var rotateSpeed = -60; 
-var imgWidth = 120; 
-var imgHeight = 170;
-
-var bgMusicURL = 'sound/musik.mp3';
-var bgMusicControls = true;
-
-setTimeout(init, 1000);
-
-var odrag = document.getElementById('drag-container');
-var ospin = document.getElementById('spin-container');
-var aImg = ospin.getElementsByTagName('img');
-var aVid = ospin.getElementsByTagName('video');
-var aEle = [...aImg, ...aVid];
-
-
-ospin.style.width = imgWidth + "px";
-ospin.style.height = imgHeight + "px";
-
-
-var ground = document.getElementById('ground');
-ground.style.width = radius * 3 + "px";
-ground.style.height = radius * 3 + "px";
-
-function init(delayTime) {
-  for (var i = 0; i < aEle.length; i++) {
-    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
-    aEle[i].style.transition = "transform 1s";
-    aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
-  }
+// Fetch Nama Lengkap based on NIK
+function getNamaByNIK(nik) {
+  var sheet = SpreadsheetApp.openById('1zRyrNT10ZVb0yPanQh_AGsCODeD9CNVIM7fBhS0u-Tk');
+  var dataSheet = sheet.getSheetByName('Data Karyawan');
+  var data = dataSheet.getRange(2, 1, dataSheet.getLastRow() - 1, 2).getValues();
+  var nikToNameMap = Object.fromEntries(data.map(row => [row[0], row[1]]));
+  return nikToNameMap[nik] || "NIK tidak ditemukan";
 }
 
-function applyTranform(obj) {
-  // Constrain the angle of camera (between 0 and 180)
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
-
-  // Apply the angle
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+// Function to handle form submission
+function submitFormData(formData) {
+  var sheet = SpreadsheetApp.openById('1zRyrNT10ZVb0yPanQh_AGsCODeD9CNVIM7fBhS0u-Tk');
+  var dataSheet = sheet.getSheetByName('Data Karyawan');
+  
+  // Add data to the next row
+  dataSheet.appendRow([
+    formData.nik,
+    formData.name,
+    formData.ktp,
+    formData.phone,
+    formData.address,
+    // Add other form fields here
+  ]);
 }
-
-function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
-}
-
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
-
-// auto spin
-if (autoRotate) {
-  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
-  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
-}
-
-// add background music
-if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" autoplay loop>    
-<p>If you are reading this, it is because your browser does not support the audio element.</p>
-</audio>
-`;
-}
-
-// setup events
-document.onpointerdown = function (e) {
-  clearInterval(odrag.timer);
-  e = e || window.event;
-  var sX = e.clientX,
-      sY = e.clientY;
-
-  this.onpointermove = function (e) {
-    e = e || window.event;
-    var nX = e.clientX,
-        nY = e.clientY;
-    desX = nX - sX;
-    desY = nY - sY;
-    tX += desX * 0.1;
-    tY += desY * 0.1;
-    applyTranform(odrag);
-    sX = nX;
-    sY = nY;
-  };
-
-  this.onpointerup = function (e) {
-    odrag.timer = setInterval(function () {
-      desX *= 0.95;
-      desY *= 0.95;
-      tX += desX * 0.1;
-      tY += desY * 0.1;
-      applyTranform(odrag);
-      playSpin(false);
-      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
-        clearInterval(odrag.timer);
-        playSpin(true);
-      }
-    }, 17);
-    this.onpointermove = this.onpointerup = null;
-  };
-
-  return false;
-};
-
-document.onmousewheel = function(e) {
-  e = e || window.event;
-  var d = e.wheelDelta / 20 || -e.detail;
-  radius += d;
-  init(1);
-};
